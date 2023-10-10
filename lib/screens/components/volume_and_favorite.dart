8db455c74d759c58916cfff2a2af91c28dfb31c3
kockono <kockonoop@gmail.com>
2023-10-10 16:12:34 -0600

    first commit

---
+++ lib/screens/components/volume_and_favorite.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../constants.dart';
+
+class VolumeAndFavorite extends StatelessWidget {
+  const VolumeAndFavorite({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          SizedBox(
+            height: 24,
+            width: 24,
+            child: Stack(
+              alignment: Alignment.center,
+              children: const [
+                CircularProgressIndicator(
+                  value: 0.4,
+                  color: Colors.white,
+                  backgroundColor: grayColor,
+                  strokeWidth: 1.2,
+                ),
+                Icon(
+                  CupertinoIcons.speaker_2_fill,
+                  color: grayColor,
+                  size: 14,
+                ),
+              ],
+            ),
+          ),
+          Container(
+            height: 24,
+            width: 24,
+            decoration: BoxDecoration(
+              shape: BoxShape.circle,
+              border: Border.all(color: grayColor, width: 1.2),
+            ),
+            child: const Icon(
+              CupertinoIcons.heart,
+              color: grayColor,
+              size: 14,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
